396efa47e260003b311a02c1f4679e21837f1e87
Rafa Kraetzer <73673435+Rafa-GitHub@users.noreply.github.com>
2024-03-03 15:05:00 -0300

    classe Candidato
    criação da classe Candidato

---
+++ entra21Blusoft_ws/sistema_votacao/src/entidades/Candidato.java
@@ -0,0 +1,14 @@
+package entidades;
+
+public class Candidato {
+	
+	String name;
+	
+	public Candidato() {};
+	
+	public Candidato(String name) {
+		this.name = name;
+	}
+	
+	
+}
--- entra21Blusoft_ws/sistema_votacao/src/sistema_votacao/ProgramaVotação.java
+++ entra21Blusoft_ws/sistema_votacao/src/sistema_votacao/ProgramaVotação.java
@@ -3,8 +3,12 @@ package sistema_votacao;
 public class ProgramaVotação {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("ola");
+		
+		Candidatos
+		
+		
 	}
 
 }
