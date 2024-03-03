ffea4215114b2e86cf0c74bce775f9c6bb522dbd
Rafa Kraetzer <73673435+Rafa-GitHub@users.noreply.github.com>
2024-03-03 10:05:57 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projetos
+ projetosEntra21
---
+++ entra21Blusoft_ws/aulasEntra21/src/aulasEntra21/TesteAula.java
@@ -0,0 +1,14 @@
+package aulasEntra21;
+
+public class TesteAula {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ola mundo!");
+		
+		
+		
+
+	}
+
+}
